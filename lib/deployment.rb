43c0c477ecede7852d0ad708be5a61c7024a0a94
Sascha Teske <sascha.teske@gmail.com>
2012-04-26 00:26:35 +0200

    Fix tag setting

--- lib/deployment.rb
+++ lib/deployment.rb
@@ -3,14 +3,17 @@ class Deployment
     puts "\x1b[0m\x1b[34m"
     print "Starting deployment of #{CONFIG.branch} to #{CONFIG.target} ..."
     CONFIG.remote_run CONFIG.prescript if CONFIG.prescript?
+
     CONFIG.remote_run %Q(
       cd #{CONFIG.target}/current
       unset GIT_DIR
       git pull origin #{CONFIG.branch}
     )
+
     CONFIG.remote_run CONFIG.postscript if CONFIG.postscript?
 
     %x(
+      unset GIT_DIR
       cd #{CONFIG.repo}
       git tag #{CONFIG.tag}
     ) if CONFIG.tag?
